48d9138ac72739ced13cdd798d1bd50c2b7373a5
vanamulakshmi <adhilakshmivanamu@gmail.com>
2025-09-09 12:53:54 +0530

    Add files via upload

---
+++ ifelse.js
@@ -0,0 +1,19 @@
+let marks=100;// Declare the variable
+let result;
+if(marks>=90 &&marks<=100){
+    result="A+";
+}else if(marks>=80 &&marks<90){
+    result="A";
+}else if(marks>=70 &&marks<80){
+    result="B+";
+}else if(marks>=60 &&marks<70){
+    result="B";
+}else if(marks>=50 &&marks<60){
+    result="C+";
+}else if(marks>=40 &&marks<50){
+    result="Fail";
+}
+console.log(marks);marks;
+console.log(result);result; // Output the result to the console
+    
+    
